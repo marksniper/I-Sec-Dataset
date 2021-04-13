ca37cd4d90c671e486094738a0603ead17aaf8a6
Benedetto Marco Serinelli <benedetto.serinelli@unige.ch>
2021-04-13 18:13:35 +0200

    Remove keras model due no space left

--- training_script/launch_script.sh
+++ training_script/launch_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 ncdbad_svm_model.py
-python3 ncdbad_keras_model.py
+# python3 ncdbad_keras_model.py
 python3 ncdbad_randomforest_model.py
 python3 ncdbad_xgboost_gradient_model.py
