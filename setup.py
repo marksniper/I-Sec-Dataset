3e08b09a9d9ab11bbdff86971868c3f403cb0c80
Benedetto Marco Serinelli <benedetto.serinelli@unige.ch>
2021-04-13 17:53:29 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,4 @@ setup(name='ncdbad-predictor',
       url='https://isec.unige.ch/',
       platforms=['any'],
       install_requires=['joblib', 'keras', 'scikit-learn==0.23.1', 'xgboost', 'tensorflow', 'watchdog', 'pandas',
-                        'protobuf==3.12.2', 'pyzmq==19.0.1', 'numpy', 'python-socketio[client]'])
+                        'protobuf==3.12.2', 'pyzmq==19.0.1', 'numpy', 'python-socketio[client]', 'six==1.15.0', 'numpy=1.19.2','h5py=2.10.0'])
